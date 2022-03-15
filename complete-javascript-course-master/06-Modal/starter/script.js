1317c536e21bbe334decebaa45618c46f8220f1e
Xristina <xristiniw97@hotmail.com>
2022-03-15 12:30:08 +0200

    MODAL CREATE

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  console.log('Modal closed');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  btnsOpenModal[index].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// the function takes the event 
+document.addEventListener('keydown', function (e) {
+    console.log(e);
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
+
+// const closeModal = function (arg) {
+//   arg.addEventListener('click', function () {
+//     console.log('Modal closed');
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
+// };
+
+// closeModal(btnCloseModal);
+
+// closeModal(overlay);
